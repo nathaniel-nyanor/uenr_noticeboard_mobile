461b6e3e839f59e197a6781aaab9e471932b0d3b
nathaniel-nyanor <nathaniel.nyanor@gmail.com>
2024-01-02 21:21:50 +0000

    chore: Initial Commit

---
+++ android/app/src/main/kotlin/com/uenr/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.uenr.uenr_noticeboard_mobile
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_development.dart
@@ -0,0 +1,6 @@
+import 'package:uenr_noticeboard_mobile/app/app.dart';
+import 'package:uenr_noticeboard_mobile/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
