9acf3fcd275f75a38358e1b9d29e046c76c6a6a9
nathaniel-nyanor <nathaniel.nyanor@gmail.com>
2024-01-03 23:26:15 +0000

    fix: appwrite initialization

--- lib/app/view/app.dart
+++ lib/app/view/app.dart
@@ -1,4 +1,3 @@
-import 'package:appwrite/appwrite.dart';
 import 'package:flutter/material.dart';
 import 'package:uenr_noticeboard_mobile/counter/counter.dart';
 import 'package:uenr_noticeboard_mobile/l10n/l10n.dart';
@@ -6,15 +5,6 @@ import 'package:uenr_noticeboard_mobile/l10n/l10n.dart';
 class App extends StatelessWidget {
   const App({super.key});
 
-
-void main() {
-  Client client = Client();
-
-  client
-    .setEndpoint('https://cloud.appwrite.io/v1') // Endpoint
-    .setProject('65335295d6476703429c') // Project ID
-  ;
-}
 
   @override
   Widget build(BuildContext context) {
--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,6 +1,15 @@
+import 'package:appwrite/appwrite.dart';
 import 'package:uenr_noticeboard_mobile/app/app.dart';
 import 'package:uenr_noticeboard_mobile/bootstrap.dart';
 
 void main() {
+  
+  Client client = Client();
+
+  client
+    .setEndpoint('https://cloud.appwrite.io/v1') // Endpoint
+    .setProject('65335295d6476703429c') // Project ID
+  ;
+
   bootstrap(() => const App());
 }
