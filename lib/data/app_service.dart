a32cb2a321ee059cb6d296c42691783a582ae346
nathaniel-nyanor <nathaniel.nyanor@gmail.com>
2024-01-06 22:48:51 +0000

    feat: working on auth

--- lib/data/app_service.dart
+++ lib/data/app_service.dart
@@ -1,13 +1,13 @@
 import 'package:appwrite/appwrite.dart';
 import 'package:uenr_noticeboard_mobile/core/core.dart';
 
-class AppService{
-  
-  Future<void>appService () async {
-    Client client = Client();
+  Client client = Client();
+  late final Account account;
+class AppService {
+  Future<void> appService() async {
     client
         .setEndpoint(AppwriteConstants.appwriteEnpointUrl) // Endpoint
         .setProject(AppwriteConstants.appwriteProjectId); // Project ID
-
+    account = Account(client);
   }
 }
--- lib/features/auth/register/cubit/register_cubit.dart
+++ lib/features/auth/register/cubit/register_cubit.dart
@@ -1,9 +1,31 @@
 import 'package:appwrite/appwrite.dart';
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
+import 'package:uenr_noticeboard_mobile/data/app_service.dart';
 
 part 'register_state.dart';
 
 class RegisterCubit extends Cubit<RegisterState> {
   RegisterCubit() : super(RegisterInitial());
+
+  final account = Account(client);
+
+  Future<void> signUp({
+    required String email,
+    required String password,
+  }) async {
+    try {
+      await account.create(
+        userId: ID.unique(),
+        email: email,
+        password: password,
+      );
+      final response = await account.createEmailSession(
+        email: email,
+        password: password,
+      );
+    } catch (e) {
+      rethrow;
+    }
+  }
 }
